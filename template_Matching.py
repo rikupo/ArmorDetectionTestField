6b1bc81d296cf2cd0fbc747d81ff497743c6c457
rikumac <ageoftofu@gmail.com>
2022-03-24 13:21:25 +0900

    Add some code frames.

---
+++ template_Matching.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy
+
+
+def main():
+    # load the image from lists or folder
+    return 0
+
+
+def train():
+    return None
+
+
+def test():
+    return None
+
+
+def load_specified_images():
+    return None
+
+
+if __name__ == '__main__':
+    main()
