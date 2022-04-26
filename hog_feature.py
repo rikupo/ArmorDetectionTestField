e3f915a592c9ac19abaa85001d0936274bd348d9
rikupo <ageoftofu@gmail.com>
2022-04-26 18:08:38 +0900

    add hog

---
+++ hog_feature.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy
+import dlib
+import xgboost as xgb
+
+
+def main():
+    print("100")
+    return 0
+
+
+def svm():
+    pass
+
+
+def xgboost():
+    pass
+
+
+if __name__ == "__main__":
+    main()
